509dc433ee5517d023a443fbb672abd35da3662a
David Rolfe <drolfe@voltdb.com>
2020-11-27 14:54:03 +0000

    uncommented sqlcmd

--- scripts/setup_on_aws.sh
+++ scripts/setup_on_aws.sh
@@ -5,6 +5,6 @@
 cd
 cd voltdb-c4d/ddl
 
-#sqlcmd --servers=vdb1 < db.sql
+sqlcmd --servers=vdb1 < db.sql
 
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-c4d/scripts/export_and_import.xml
