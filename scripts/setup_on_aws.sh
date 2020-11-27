5426af1a45c1c16e9df69fd5a2191fcbd32c0792
David Rolfe <drolfe@voltdb.com>
2020-11-27 13:49:19 +0000

    aws setup

---
+++ scripts/setup_on_aws.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. $HOME/.profile
+
+cd
+cd voltdb-c4d/ddl
+
+#sqlcmd --servers=vdb1 < db.sql
+
+java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-c4d/scripts/export_and_import.xml
