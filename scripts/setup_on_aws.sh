8107add9250b33cf32281ae845c86c04716b6749
David Rolfe <drolfe@voltdb.com>
2020-11-30 20:32:40 +0000

    c4d to clite

--- scripts/setup_on_aws.sh
+++ scripts/setup_on_aws.sh
@@ -3,8 +3,8 @@
 . $HOME/.profile
 
 cd
-cd voltdb-c4d/ddl
+cd voltdb-clite/ddl
 
 sqlcmd --servers=vdb1 < create_db.sql
 
-java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-c4d/scripts/export_and_import.xml
+java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment $HOME/voltdb-clite/scripts/export_and_import.xml
